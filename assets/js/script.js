58547a8b84a2822c4de1ef8662dbf69a6b02d751
Arthur Gomes <153852894+mercuriohg@users.noreply.github.com>
2025-10-06 23:18:35 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,12 @@ $(document).ready(function(){
         window.open('assets/pages/vendas.html', '_self') 
     })
     
-})
+
+   $("#nav-principal").ready(function () {
+       $("#menu-toggle").click(function () {
+       $("#menu-links").slideToggle(300);
+  });
+
+
+ 
+})})
